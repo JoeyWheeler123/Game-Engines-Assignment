86061dd27dbd2148e777f8ebd8516a71ceece4b2
Connor Ryan <c16755215@mydit.ie>
2018-11-24 14:28:06 +0000

    2D fractal tree

--- Game Engines Assignment/Assets/Scripts/FractalTree.cs
+++ Game Engines Assignment/Assets/Scripts/FractalTree.cs
@@ -8,6 +8,9 @@ public class FractalTree : MonoBehaviour
 	public int sections = 6;
 	public int branches = 2;
 
+	public float scale = .5f;
+	public float angle = 30;
+
 	// Use this for initialization
 	void Start()
 	{
@@ -33,8 +36,9 @@ public class FractalTree : MonoBehaviour
 
 	public void Grown(int index)
 	{
-		gameObject.transform.position += transform.up;
-		gameObject.transform.rotation *= Quaternion.Euler(30, 0, 0);
+		gameObject.transform.position += transform.up * gameObject.transform.localScale.y;
+		gameObject.transform.rotation *= Quaternion.Euler(angle * ((index * 2) -1), 0, 0);
+		gameObject.transform.localScale *= scale;
 	}
 
 }
