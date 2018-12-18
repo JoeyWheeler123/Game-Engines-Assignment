4191d39fbccc19fffd1e1774defad6bffdc3b442
Connor Ryan <c16755215@mydit.ie>
2018-12-18 21:56:50 +0000

    Updated spawning

--- Game Engines Assignment/Assets/Scripts/Grow.cs
+++ Game Engines Assignment/Assets/Scripts/Grow.cs
@@ -4,8 +4,28 @@ using UnityEngine;
 
 public class Grow : MonoBehaviour {
 
-    public void Grown()
-	{
-		gameObject.transform.position += Vector3.up;
-	}
+    public float radius = 1;
+    public Vector2 regionSize = Vector2.one;
+    public int rejectionSamples = 30;
+    public float displayRadius = 1;
+
+    List<Vector2> points;
+
+    private void OnValidate()
+    {
+        points = Sampling.Points(radius, regionSize, rejectionSamples);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(regionSize / 2, regionSize);
+        if(points != null)
+        {
+            foreach(Vector2 point in points)
+            {
+                Gizmos.DrawWireSphere(point, displayRadius);
+            }
+        }
+
+    }
 }
