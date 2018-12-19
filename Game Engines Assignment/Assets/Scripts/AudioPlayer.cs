d3add6694fe4f27090510204adc6b5ff6f202a12
Connor Ryan <c16755215@mydit.ie>
2018-12-19 21:17:25 +0000

    some audio reactive stuff

---
+++ Game Engines Assignment/Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioPlayer : MonoBehaviour {
+
+    AudioSource audioSource;
+
+    public static float[] samples = new float[512];
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        GetSpectrumAudioSource();
+    }
+
+    void GetSpectrumAudioSource()
+    {
+        audioSource.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+    }
+}
---
+++ Game Engines Assignment/Assets/Scripts/AudioVisual.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioVisual : MonoBehaviour {
+
+    GameObject target;
+    public float maxScale = 2;
+
+	// Use this for initialization
+	void Start ()
+    {
+        target = gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		for(int i = 0; i < 512; i++)
+        {
+            target.transform.localScale = new Vector3(2, (AudioPlayer.samples[i] * maxScale) + 2, 2);
+        }
+	}
+}
